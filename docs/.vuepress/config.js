cf230ef2c26953f37249f64ecdd2398d455924b9
lifuhai1106 <1109372147@qq.com>
2021-01-04 15:20:42 +0800

    首页

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: 'vue-tagtree',
+    themeConfig: {
+        nav: [
+            { text: '指南', link: '/' },
+            { text: 'GitHub', link: 'https://github.com/LiFuHai1106/vue-tagtree' },
+        ]
+    }
+}
